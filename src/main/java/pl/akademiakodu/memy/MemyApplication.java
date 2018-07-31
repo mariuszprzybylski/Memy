c97342091bc743a7db595c0dfc387785ed479138
Mariusz <mariusz_przybylski@wp.pl>
2018-07-31 09:31:10 +0200

    first commit

---
+++ src/main/java/pl/akademiakodu/memy/MemyApplication.java
@@ -0,0 +1,12 @@
+package pl.akademiakodu.memy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemyApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/akademiakodu/memy/MemyApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.akademiakodu.memy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
