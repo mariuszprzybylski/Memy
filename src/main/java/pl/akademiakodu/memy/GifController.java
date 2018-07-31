d28a080614fcb0a02a7479611e272f67c5298296
Mariusz <mariusz_przybylski@wp.pl>
2018-07-31 09:55:32 +0200

    I added a front-end application

---
+++ src/main/java/pl/akademiakodu/memy/GifController.java
@@ -0,0 +1,16 @@
+package pl.akademiakodu.memy;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GifController {
+
+
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+
+}
