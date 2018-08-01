09f90b614c3181211185818be67b20b0c4e4ab53
Paulina Wójcik <paulina.wojcikk@o2.pl>
2018-08-01 16:01:02 +0200

    added uploading

--- src/main/java/pl/akademiakodu/memy/MemyApplication.java
+++ src/main/java/pl/akademiakodu/memy/MemyApplication.java
@@ -1,12 +1,26 @@
 package pl.akademiakodu.memy;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import pl.akademiakodu.memy.storage.StorageProperties;
+import pl.akademiakodu.memy.storage.StorageService;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class MemyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MemyApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
 	}
 }
